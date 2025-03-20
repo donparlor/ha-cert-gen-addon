d655078b74e63686ef084a6b1bea81b87b4259cd
donparlor <97048708+donparlor@users.noreply.github.com>
2025-03-20 00:26:28 -0400

    Update run.sh

--- cert_gen/run.sh
+++ cert_gen/run.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/ash
-echo "Starting cert_gen add-on..."
+echo "Starting cert_gen add-on test..."
 
-# Juste pour vérifier qu'on est dans le conteneur
+# Tester la présence d'OpenSSL
+echo "OpenSSL version:"
 openssl version
 
-# Exécution du script de génération
-if [ -f /config/scripts/generate_p12_and_cer.sh ]; then
-  echo "Running generate_p12_and_cer.sh..."
-  bash /config/scripts/generate_p12_and_cer.sh
-fi
-
-# Exécution du script de compression
-if [ -f /config/scripts/compress_certificate.sh ]; then
-  echo "Running compress_certificate.sh..."
-  bash /config/scripts/compress_certificate.sh
-fi
-
-echo "All done! Keeping container alive..."
-tail -f /dev/null
+# Sortir tout de suite
+echo "Done testing. Exiting."
